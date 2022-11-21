4b89f2108aa2934361986e7b237052e8689b5bc6
Rajdip <rajdip.ghosh@xempla.io>
2022-11-21 13:38:14 +0530

    updated

--- backend/DB_Connect/dbcon.js
+++ backend/DB_Connect/dbcon.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose")
 
 
-// const DBUrl = `mongodb+srv://rajdip:9842@cluster0.8zohx.mongodb.net/MeterDB?retryWrites=true&w=majority`
-const DBUrl = `mongodb://localhost:27017/MeterAPP`
+const DBUrl = `mongodb+srv://rajdip:9842@cluster0.8zohx.mongodb.net/MeterDB?retryWrites=true&w=majority`
+// const DBUrl = `mongodb://localhost:27017/MeterAPP`
 
 mongoose.connect(DBUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("DB connection Successfull");
